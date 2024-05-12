ab6c4c9476bc918a1f2cb9f1217a84a9dcc1b5a5
Dean <deannreid@hotmail.co.uk>
2024-05-12 14:44:14 +0100

    Who doesn't like colours

--- RS2NM.py
+++ RS2NM.py
@@ -16,21 +16,55 @@
 #       - If no location selected then it will default to the pwd
 # python3 rs2nm.py <Target> <FS Location>
 
-# Code Version: 1.1
+# Code Version: 1.0
 #
 # Updates:
 # 12/05/2024: Initial Code Build
-#             What is capital letters
+#             Who Doesn't Like Colours'
+#             Added Help Bit just in case
 #
 
 import subprocess
 import sys
 import os
+import colorama
+from colorama import Fore, Style
+
+def print_help():
+    print("""
+    RustScan 2 NMap - Scottish Edition
+    Developed by Dean with a bit of love
+
+    Script Name: rs2nm.py
+
+    Script Information:
+
+    Just a script to save 2 seconds that will automagically grab Rustscan ports and submit them
+    to NMAP for some further detailed anal.lysis...
+
+    Arguments:
+      - Target (NO FLAG REQUIRED JUST IP)
+      - File Save Location
+          - If no location selected then it will default to the pwd
+      - Help: Use '-h' or '--help' to display help information
+
+    Code Version: 1.1
+
+    Updates:
+    12/05/2024: Initial Code Build
+                Who Doesn't Like Colours'
+                Added Help Bit just in-case
+    """)
 
 def main():
+    # Check if help argument is provided
+    if len(sys.argv) > 1 and (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+        print_help()
+        sys.exit(0)
+
     # Check target is provided
     if len(sys.argv) < 2:
-        print("Requires: {} <Target> [save_location] you dumpling".format(sys.argv[0]))
+        print(Fore.RED + "Requires: {} <Target> [save_location] you dumpling".format(sys.argv[0]) + Style.RESET_ALL)
         sys.exit(1)
 
     # Assign the The_Bad_Guy from the argument
@@ -39,14 +73,15 @@ def main():
     # Set the default output directory to current directory if not provided
     save_location = os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
 
-    print("Just gonnae run a quick wee rustscan test \n You selected: {}...\n Estimated Time Remaining: 1h 35m\n\n Just kidding, only going to take a few seconds, they say".format(The_Bad_Guy))
+    print(Fore.YELLOW + "Just gonnae run a quick wee rustscan test\nYou selected: {}".format(Fore.RED + The_Bad_Guy + Style.RESET_ALL))
+    print(Fore.YELLOW + "Estimated Time Remaining: 1h 35m\n\nJust kidding, only going to take a few seconds, they say" + Style.RESET_ALL)
 
     # Find open ports
     rustscan_process = subprocess.Popen(["rustscan", "-g", "-a", The_Bad_Guy, "--ulimit", "5000"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     rustscan_output, rustscan_error = rustscan_process.communicate()
 
     if rustscan_error:
-        print("\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run RustScan:\n", rustscan_error.decode())
+        print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run RustScan:\n", rustscan_error.decode()+Style.RESET_ALL)
         sys.exit(1)
 
     # Extract ports
@@ -57,10 +92,10 @@ def main():
             break
 
     if not open_ports:
-        print("\n\nCanny find anything, tough luck, see you next week.")
+        print(Fore.CYAN + "\n\nCanny find anything, tough luck, see you next week."+Style.RESET_ALL)
         sys.exit(0)
 
-    print("\n\nOOoh, There are a few ports open \n Gonnae copy these to NMAP for ye, for some intricate.... ;)  scanning... \n\n ({})".format(open_ports))
+    print(Fore.GREEN + "\n\nOOoh, There are a few ports open \nGonnae copy these to NMAP for ye, for some intricate.... ;)  scanning...\n({})".format(Fore.MAGENTA + open_ports + Style.RESET_ALL) + Style.RESET_ALL)
 
     # Run Nmap
     nmap_output_file = os.path.join(save_location, "{}_nmap_results".format(The_Bad_Guy))
@@ -68,11 +103,12 @@ def main():
     nmap_output, nmap_error = nmap_process.communicate()
 
     if nmap_error:
-        print("\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run NMap:", nmap_error.decode())
+        print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run NMap:", nmap_error.decode()+Style.RESET_ALL)
         sys.exit(1)
 
     print(nmap_output.decode())
-    print("I've saved your loot here:", nmap_output_file)
+    print(Fore.GREEN + "I've saved your loot here:", nmap_output_file + Style.RESET_ALL)
+
 
 if __name__ == "__main__":
     main()
