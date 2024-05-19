ff181f0cd3e18777dc396d11a8572657f245dc38
Dean Reid <deannreid@hotmail.co.uk>
2024-05-20 00:44:22 +0100

    Added Features
    Feature, 1,2,3 and 4 testing

--- RS2NM.py
+++ RS2NM.py
@@ -6,17 +6,15 @@
 # Script Name: rs2nm.py
 #
 # Script Information:
-#
-# Just a script to save 2 seconds that will automagically grab Rustscan ports and submit them
-# to NMAP for some further detailed analysis...
+# Just a script to save 2 seconds that will automagically grab Rustscan ports and submit them to NMAP for some further detailed analysis...
 #
 # Arguments:
 #   - Target
 #   - File Save Location
 #       - If no location is selected then it will default to the pwd
 # python3 rs2nm.py <Target> <FS Location>
-
-# Code Version: 1.7.1
+#
+# Code Version: 1.1
 #
 # Updates:
 # 12/05/2024: Initial Code Build
@@ -25,6 +23,8 @@
 # 18/05/2024: Added Host Discovery Blocker Fix
 # 19/05/2024: Added Automatic Domain Append to /etc/hosts
 #             Improved Domain Extraction with Regular Expressions
+# 20/05/2024: Added MultiOS Support - Win/Linux
+#             Added Admin/Sudo Check to for adding to host file
 #
 
 import subprocess
@@ -33,6 +33,7 @@ import os
 import colorama
 from colorama import Fore, Style
 import re
+import ctypes
 
 def print_help():
     print("""
@@ -42,26 +43,32 @@ def print_help():
     Script Name: rs2nm.py
 
     Script Information:
-
-    Just a script to save 2 seconds that will automagically grab Rustscan ports and submit them
-    to NMAP for some further detailed analysis...
+    Just a script to save 2 seconds that will automagically grab Rustscan ports and submit them to NMAP for some further detailed analysis...
 
     Arguments:
       - Target (NO FLAG REQUIRED JUST IP)
       - File Save Location
           - If no location is selected then it will default to the pwd
       - Help: Use '-h' or '--help' to display help information
-
-    Code Version: 1.7.1
-
-    Updates:
-    12/05/2024: Initial Code Build
-                Who Doesn't Like Colours'
-                Added Help Bit just in case
-    18/05/2024: Added Host Discovery Blocker Fix
-    19/05/2024: Added Automatic Domain Append to /etc/hosts
-                Improved Domain Extraction with Regular Expressions
+          
     """)
+
+def rootChecker():
+    if os.name == 'nt':
+        # Check for admin privileges on Windows
+        try:
+            is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+        except Exception as e:
+            print(Fore.RED + "Could not determine admin privileges on Windows: " + str(e) + Style.RESET_ALL)
+            sys.exit(1)
+        if not is_admin:
+            print(Fore.RED + "This script requires administrative privileges. Please run it as an administrator." + Style.RESET_ALL)
+            sys.exit(1)
+    else:
+        # Check for sudo on Linux
+        if os.geteuid() != 0:
+            print(Fore.RED + "This script requires sudo privileges. Please run it with sudo." + Style.RESET_ALL)
+            sys.exit(1)
 
 def run_nmap(nmap_command):
     nmap_process = subprocess.Popen(nmap_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -69,10 +76,10 @@ def run_nmap(nmap_command):
     return nmap_output.decode(), nmap_error.decode()
 
 def add2Hosts(nmap_output, target_ip):
-    print(Fore.CYAN + "Checking for open port 389 and extracting domain information..." + Style.RESET_ALL)
+    print(Fore.CYAN + "Checking for open Active Directory ports and extracting domain information..." + Style.RESET_ALL)
     for line in nmap_output.split('\n'):
-        if '389/tcp' in line and 'open' in line and 'Microsoft Windows Active Directory LDAP' in line:
-            print(Fore.GREEN + "Looks like a Domain. Port 389 found with LDAP information:\n" + line + Style.RESET_ALL)
+        if any(port in line for port in ['389/tcp', '636/tcp', '3268/tcp', '3269/tcp']) and 'open' in line and 'Microsoft Windows Active Directory LDAP' in line:
+            print(Fore.GREEN + "Active Directory LDAP information found:\n" + line + Style.RESET_ALL)
             # Extract domain from the line using regular expressions
             domain_match = re.search(r'Domain: ([^\s,]+)', line)
             if domain_match:
@@ -80,37 +87,35 @@ def add2Hosts(nmap_output, target_ip):
                 print(Fore.GREEN + f"Extracted domain: {domain}" + Style.RESET_ALL)
                 with open('/etc/hosts', 'a') as hosts_file:
                     hosts_file.write(f"{target_ip}\t{domain}\n")
-                print(Fore.GREEN + f"Added {domain} to /etc/hosts with IP {target_ip}" + Style.RESET_ALL)
+                print(Fore.GREEN + f"Added {domain} to the host file with IP {target_ip}" + Style.RESET_ALL)
                 return
-    print(Fore.RED + "No relevant domain information found for port 389." + Style.RESET_ALL)
+    print(Fore.RED + "No relevant domain information found for any Active Directory ports." + Style.RESET_ALL)
 
 def main():
     try:
-        # Check if help argument is provided
+        rootChecker()
+
+        # --help Argument
         if len(sys.argv) > 1 and (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
             print_help()
             sys.exit(0)
 
         # Check target is provided
-        if len(sys.argv) < 2:
-            print(Fore.RED + "Requires: {} <Target> [save_location] you dumpling".format(sys.argv[0]) + Style.RESET_ALL)
+        if len(sys.argv) < 7:
+            print(Fore.RED + f"Requires: {sys.argv[0]} <Target> [save_location] you dumpling" + Style.RESET_ALL)
             sys.exit(1)
 
-        # Assign the The_Bad_Guy from the argument
-        The_Bad_Guy = sys.argv[1]
-
-        # Set the default output directory to current directory if not provided
+        target = sys.argv[1]
         save_location = os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
 
-        print(Fore.YELLOW + "Just gonnae run a quick wee rustscan test\nYou selected: {}".format(Fore.RED + The_Bad_Guy + Style.RESET_ALL))
-        print(Fore.YELLOW + "Estimated Time Remaining: 1h 35m\n\nJust kidding, only going to take a few seconds, they say" + Style.RESET_ALL)
+        print(Fore.YELLOW + "Just gonnae run a quick wee rustscan test\nYou selected: " + Fore.RED + target + Style.RESET_ALL)
+        print(Fore.YELLOW + "Estimated Time Remaining: 1h 35m...\nJust kidding, only going to take a few seconds, they say" + Style.RESET_ALL)
 
-        # Find open ports
-        rustscan_process = subprocess.Popen(["rustscan", "-g", "-a", The_Bad_Guy, "--ulimit", "5000"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        rustscan_process = subprocess.Popen(["rustscan", "-g", "-a", target, "--ulimit", "10000"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         rustscan_output, rustscan_error = rustscan_process.communicate()
 
         if rustscan_error:
-            print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run RustScan:\n", rustscan_error.decode() + Style.RESET_ALL)
+            print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run RustScan:\n" + rustscan_error.decode() + Style.RESET_ALL)
             sys.exit(1)
 
         # Extract ports
@@ -124,11 +129,10 @@ def main():
             print(Fore.CYAN + "\n\nCanny find anything, tough luck, see you next week." + Style.RESET_ALL)
             sys.exit(0)
 
-        print(Fore.GREEN + "\n\nOOoh, There are a few ports open \nGonnae copy these to NMAP for ye, for some intricate.... ;)  scanning...\n({})".format(Fore.MAGENTA + open_ports + Style.RESET_ALL) + Style.RESET_ALL)
+        print(Fore.GREEN + "\n\nOOoh, There are a few ports open \nGonnae copy these to NMAP for ye, for some intricate.... ;)  scanning...\n(" + Fore.MAGENTA + open_ports + Style.RESET_ALL + ")" + Style.RESET_ALL)
 
-        # Run Nmap
-        nmap_output_file = os.path.join(save_location, "{}_nmap_results".format(The_Bad_Guy))
-        nmap_command = ["nmap", "-p", open_ports, "-sC", "-sV", "-oA", nmap_output_file, The_Bad_Guy]
+        nmap_output_file = os.path.join(save_location, f"{target}_nmap_results")
+        nmap_command = ["nmap", "-p", open_ports, "-sC", "-sV", "-oA", nmap_output_file, target]
 
         nmap_output, nmap_error = run_nmap(nmap_command)
         if "Note: Host seems down. If it is really up, but blocking our ping probes, try -Pn" in nmap_output:
@@ -137,14 +141,13 @@ def main():
             nmap_output, nmap_error = run_nmap(nmap_command)
 
         if nmap_error:
-            print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run NMap:", nmap_error + Style.RESET_ALL)
+            print(Fore.RED + "\n\nExcuse me pal\n Someone(probably dean) fucked up because I canny run NMap:" + nmap_error + Style.RESET_ALL)
             sys.exit(1)
 
         print(nmap_output)
-        print(Fore.GREEN + "I've saved your loot here:", nmap_output_file + Style.RESET_ALL)
+        print(Fore.GREEN + "I've saved your loot here: " + nmap_output_file + Style.RESET_ALL)
 
-        # Check and update /etc/hosts if port 389 is open and contains domain info
-        add2Hosts(nmap_output, The_Bad_Guy)
+        add2Hosts(nmap_output, target)
 
     except KeyboardInterrupt:
         while True:
@@ -159,8 +162,7 @@ def main():
                 print(Fore.YELLOW + "\n\nMaybe you should read the instructions... \n Exiting...." + Style.RESET_ALL)
                 sys.exit(0)
             else:
-                print(Fore.RED + "\n\nInvalid input. Please enter 'y' or 'n'." + Style.RESET_ALL)
-                continue
+                print(Fore.RED + "\n\nInvalid input. Please enter 'k' or 'n'." + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
